b2896a76496be90aec9f4a2ca426e252b6f21502
Caleb Tony <calebmbugua@gmail.com>
2021-03-04 14:44:26 +0300

    added advanced functions

--- prototypes.js
+++ prototypes.js
@@ -6,3 +6,15 @@ function main() {
 
   console.log(aNumber.addTen());
 }
+
+function advFunctions() {
+  const advFn = function (fn) {
+    fn();
+  };
+
+  advFn(function main() {
+    const aNumber = 30;
+
+    console.log("advanced function output: ", aNumber.addTen());
+  });
+}
